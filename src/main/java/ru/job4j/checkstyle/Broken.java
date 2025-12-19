eef0d610a859a05d8f916259bb9c77a8030050e1
VasilyevVD <kobe001@mail.ru>
2025-12-19 16:13:24 +0400

    6. Правила Checkstyle [#504981 #526692]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+    public class Broken {
+        public static final String NEWVALUE = "";
+        private String surname;
+        private String name;
+        private int sizeOfEmpty = 10;
+
+        public void broken() { }
+
+        void echo() {
+        }
+
+        public void media(Object obj) {
+            if (obj != null) {
+                System.out.println(obj);
+            }
+        }
+
+       public void method(int a, int b, int c, int d, int e, int f, int g) {
+        }
+}
+
