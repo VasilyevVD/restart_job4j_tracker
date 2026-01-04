eba19c14c0ac2196396a038f0135ab86a3cfc363
VasilyevVD <kobe001@mail.ru>
2026-01-05 00:05:55 +0400

    6.1. Вложенные классы: Внутренние классы [#504950 #530020]

---
+++ src/main/java/ru/job4j/oop/Car.java
@@ -0,0 +1,46 @@
+package ru.job4j.oop;
+
+public class Car {
+    private String brand;
+    private String model;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public void startEngine() {
+        System.out.println("Двигатель запущен");
+    }
+
+    public class Transmission {
+
+        public void accelertr() {
+            System.out.println("Ускорение");
+        }
+    }
+
+    public class Brakes {
+
+        public void brake() {
+            System.out.println("торможение");
+        }
+    }
+
+    public class TripComputer {
+        private String model = "Модель TripComputer";
+
+        public String tripDate = "Бортовой компьютер";
+
+        public void getInfo() {
+            System.out.println("Модель TripComputer: " + this.model);
+            System.out.println("Модель Car: " + Car.this.model);
+        }
+    }
+
+    public static TripComputer getTripComputer() {
+        Car car = new Car("Марка", "Модель");
+        Car.TripComputer tripComputer = car.new TripComputer();
+        return tripComputer;
+    }
+}
