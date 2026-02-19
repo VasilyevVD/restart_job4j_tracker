e6916005ba23d80336543c609cb977c0939c47e7
VasilyevVD <kobe001@mail.ru>
2026-02-19 13:13:58 +0400

    3.2. Удаление моделей из массива. [#310016 #531449]

--- src/main/java/ru/job4j/pojo/License.java
+++ src/main/java/ru/job4j/pojo/License.java
@@ -1,12 +1,30 @@
 package ru.job4j.pojo;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class License {
     private String owner;
     private String model;
     private String code;
     private LocalDate created;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        License license = (License) object;
+        return Objects.equals(owner, license.owner) && Objects.equals(model, license.model) && Objects.equals(code, license.code) && Objects.equals(created, license.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(owner, model, code, created);
+    }
 
     public String getOwner() {
         return owner;
