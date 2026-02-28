a95a3127d52c51a9cb0ca4a9625aa642902a97d3
VasilyevVD <kobe001@mail.ru>
2026-03-01 00:11:16 +0400

    7. Метод удаления заявки Tracker.delete [#211749 #531855]

--- src/main/java/ru/job4j/Tracker.java
+++ src/main/java/ru/job4j/Tracker.java
@@ -59,8 +59,8 @@ public class Tracker {
         int index = indexOf(id);
         if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
         }
-        items[size - 1] = null;
-        size--;
     }
 }
