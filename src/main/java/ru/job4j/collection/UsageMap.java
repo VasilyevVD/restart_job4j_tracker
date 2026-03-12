1a28731bd175372c97f9956747d08115c6872602
VasilyevVD <kobe001@mail.ru>
2026-03-12 14:48:38 +0400

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("tra-la-la@yandex.ru", "Ivan Petrov");
+      /*  map.remove("parsentev@yandex.ru");*/
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+        System.out.println();
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+    }
+}
