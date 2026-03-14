d13430481f1dd18df59cb89bdc3fcbf37774fd43
VasilyevVD <kobe001@mail.ru>
2026-03-14 12:44:42 +0400

    1. Паспорт и жители [#212656 #560009]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PassportOfficeTest {
+
     @Test
     public void whenTestAddMethod() {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
@@ -11,6 +12,7 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
     @Test
     public void whenTest() {
         Citizen citizen1 = new Citizen("1234", "Petr Ivanov");
@@ -20,6 +22,5 @@ public class PassportOfficeTest {
         office.add(citizen2);
         boolean result = office.add(citizen2);
         assertThat(result).isFalse();
-
     }
 }
